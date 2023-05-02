bc139f6f914090088eba5012b10a6a58d249b03d
JakariaJishan <jakariajishan987@gmail.com>
2023-05-03 02:49:04 +0600

    Add test cases for book

--- classes/book.rb
+++ classes/book.rb
@@ -1,12 +1,12 @@
 require_relative 'item'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state, :publish_date
 
-  def initialize(publisher, cover_state, date, archived = 0)
-    super(date, archived)
-    @publisher = publisher
-    @cover_state = cover_state
+  def initialize(publisher, cover_state,publish_date, archived = 0)
+    super(publish_date)
+    @publisher = publisher 
+    @cover_state = cover_state 
   end
 
   def can_be_archived?
