722e2160fb77e49021cac974d925d27b815bb123
Vanessa O <vaneolipa@gmail.com>
2023-05-02 21:42:56 -0500

    Add album_name property in the MusicAlbum class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,11 +1,12 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_accessor :on_spotify, :album_name, :publish_date
 
-  def initialize(on_spotify, publish_date, archived: false)
+  def initialize(on_spotify, publish_date, album_name, archived: false)
     super(publish_date, archived: archived)
     @on_spotify = on_spotify
+    @album_name = album_name
   end
 
   private
