8e385571c10bf97613453d7744853363651af5e4
Vanessa O <vaneolipa@gmail.com>
2023-05-02 23:14:36 -0500

    Modify file path

--- modules/load_data.rb
+++ modules/load_data.rb
@@ -2,27 +2,27 @@ require 'json'
 
 module LoadData
   def load_music
-    if File.exist?('./music_albums.json') && !File.empty?('./music_albums.json')
-      json_string = File.read('music_albums.json')
+    if File.exist?('./data/music_albums.json') && !File.empty?('./data/music_albums.json')
+      json_string = File.read('./data/music_albums.json')
       data = JSON.parse(json_string)
       @albums = data.map do |a|
         MusicAlbum.new(a['on_spotify'], a['publish_date'], a['album_name'] )
       end
     else
-      File.write('music_albums.json', JSON.generate([])) unless File.exist?('music_albums.json')
+      File.write('./data/music_albums.json', JSON.generate([])) unless File.exist?('./data/music_albums.json')
   
     end
   end
 
   def load_genre
-    if File.exist?('./genres.json') && !File.empty?('./genres.json')
-      json_string = File.read('genres.json')
+    if File.exist?('./data/genres.json') && !File.empty?('./data/genres.json')
+      json_string = File.read('./data/genres.json')
       data = JSON.parse(json_string)
       @genres = data.map do |g|
         Genre.new(g['name'])
       end
     else
-      File.write('genres.json', JSON.generate([])) unless File.exist?('genres.json')
+      File.write('./data/genres.json', JSON.generate([])) unless File.exist?('./data/genres.json')
   
     end
   end
--- modules/music_module.rb
+++ modules/music_module.rb
@@ -16,8 +16,8 @@ module MusicAlbumModule
     @albums << music_album
     @genres << genre
     genre_name = genre ? genre.name : nil
-    File.write('music_albums.json', JSON.pretty_generate(@albums.map { |a| { id: a.id, genre: genre_name, on_spotify: a.on_spotify, publish_date: a.publish_date, album_name: a.album_name } }))
-    File.write('genres.json', JSON.pretty_generate(@genres.map { |g| { id: g.id, name: g.name } }))
+    File.write('./data/music_albums.json', JSON.pretty_generate(@albums.map { |a| { id: a.id, genre: genre_name, on_spotify: a.on_spotify, publish_date: a.publish_date, album_name: a.album_name } }))
+    File.write('./data/genres.json', JSON.pretty_generate(@genres.map { |g| { id: g.id, name: g.name } }))
     puts "Music album added successfully"  
     puts ''
     start
