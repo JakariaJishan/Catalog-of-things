7b12590f10f387cbbc2f025599161e2b18e7cb70
Vanessa O <vaneolipa@gmail.com>
2023-05-02 21:57:19 -0500

    Add list_genres method

---
+++ modules/genre_module.rb
@@ -0,0 +1,21 @@
+require_relative '../app'
+require 'set'
+
+module GenreModule
+	def list_genres
+    if @genres.empty?
+      puts 'The list is empty'
+      puts ''
+      start
+    end
+		no_duplicate_genre = Set.new
+		@genres.each do |genre|
+			no_duplicate_genre << genre.name
+    end
+		no_duplicate_genre.each do |genre_name|
+      puts "- #{genre_name}"
+    end
+    puts ''
+    start
+  end
+end
